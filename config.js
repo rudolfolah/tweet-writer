2acf3922e026690bd394544c13387e4a9f8b91b6
Rudolf Olah <rudolf.olah.to@gmail.com>
2022-12-05 10:31:53 -0500

    created configuration file

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  completionPrompts: [
+    "Write a business tweet about",
+    "Compose a serious tweet about",
+    "Write a funny tweet about",
+    "Compose a funny tweet about",
+  ],
+  completionModel: "text-davinci-003",
+  completionMaxTokens: 256,
+  completionTemperature: 0.7,
+}
