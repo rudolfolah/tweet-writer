7c10f76fb9dd925ca20b05316fee4b049979696b
Rudolf Olah <rudolf.olah.to@gmail.com>
2022-12-15 23:16:29 -0500

    locale and theme

--- webui/src/index.js
+++ webui/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { LocaleProvider } from '@douyinfe/semi-ui';
+import en_US from '@douyinfe/semi-ui/lib/es/locale/source/en_US';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <LocaleProvider locale={en_US}>
+      <App />
+    </LocaleProvider>
   </React.StrictMode>
 );
 
@@ -15,3 +19,21 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const mql = window.matchMedia('(prefers-color-scheme: dark)');
+
+function matchMode(e) {
+  const body = document.body;
+  if (e.matches) {
+    if (!body.hasAttribute('theme-mode')) {
+      body.setAttribute('theme-mode', 'dark');
+    }
+  } else {
+    if (body.hasAttribute('theme-mode')) {
+      body.removeAttribute('theme-mode');
+    }
+  }
+}
+
+mql.addListener(matchMode);
+document.body.setAttribute('theme-mode', 'dark');
